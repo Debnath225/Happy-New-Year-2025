2c8fb6c84d3d631d0874bd7f59056f4c2ecb8fb4
Debnath <166276275+Debnath225@users.noreply.github.com>
2024-12-27 11:48:04 +0530

    Add files via upload

---
+++ open.js
@@ -0,0 +1,38 @@
+// const getValue =document.getElementById("getValue");
+//  const show =document.getElementById("show");
+// const store = getValue.value;
+// show.innerHTML="Happy Merry Christmas "+store;
+
+var countDownDate = new Date("Jan, 2025 00:00:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+    
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+    
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+  // Output the result in an element with id="demo"
+  document.getElementById("timeUp").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+    
+  // If the count down is over, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("timeUp").innerHTML = "Happy New Year 2025";
+    document.getElementById("timeUp").style.color ="red";
+    return 0;
+  }
+}, 1000);
+
+function gob(){
+  document.getElementById("gob").terget="_blank"
+}
---
+++ script.js
@@ -0,0 +1,9 @@
+const fName = document.getElementById("fName");
+
+const lName = document.getElementById("lName").innerHTML;
+console.log(lName);
+
+
+
+const ufName = fName.value;
+const ulName =lName.value;
