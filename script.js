a077342a34db4cd80603d5ca77deee255d6f002a
Debnath <166276275+Debnath225@users.noreply.github.com>
2024-12-28 21:18:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,27 @@ const lName = document.getElementById("lName");
 const getValue = document.getElementById("getValue");
 const noEntry = document.getElementById("noEntry");
 
+var countDownDate = new Date("Jan, 2025 00:00:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+    
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+    
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+  // Output the result in an element with id="demo"
+   
+    
+
 
 function onSubmit(){
   const  fVal= fName.value;
@@ -16,6 +37,7 @@ function onSubmit(){
     }
     else{
         getValue.action="open.html";
-        alert(`Happy New Year 2025 ${fVal}`);
+        alert(`Happy New Year 2025 ${fVal} ${days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s "}`);
     }
 }
