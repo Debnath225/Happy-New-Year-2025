887eb8d52f896e89f95b5c35fda7ff603f99b577
Debnath <166276275+Debnath225@users.noreply.github.com>
2024-12-28 20:31:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const noEntry = document.getElementById("noEntry");
 function onSubmit(){
     if(fName.value==="0" && lName.value==="0"){
         noEntry.innerHTML="Access denied";
-        alert('Happy New Year 2025 and please enter your name');
+        alert('Happy New Year 2025  please enter your name!');
         return 0;
     }
     else{
