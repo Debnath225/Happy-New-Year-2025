ed0644e5a68a4cb3b98f14a9aedbc744870b43b9
Debnath <166276275+Debnath225@users.noreply.github.com>
2024-12-28 21:23:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,22 +5,22 @@ const lName = document.getElementById("lName");
 const getValue = document.getElementById("getValue");
 const noEntry = document.getElementById("noEntry");
 
-var countDownDate = new Date("Jan, 2025 00:00:00").getTime();
+//var countDownDate = new Date("Jan, 2025 00:00:00").getTime();
 
 // Update the count down every 1 second
-var x = setInterval(function() {
+//var x = setInterval(function() {
 
   // Get today's date and time
-  var now = new Date().getTime();
+ // var now = new Date().getTime();
     
   // Find the distance between now and the count down date
-  var distance = countDownDate - now;
+ // var distance = countDownDate - now;
     
   // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+//  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+//  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  //var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  //var seconds = Math.floor((distance % (1000 * 60)) / 1000);
     
   // Output the result in an element with id="demo"
    
@@ -37,7 +37,6 @@ function onSubmit(){
     }
     else{
         getValue.action="open.html";
-        alert(`Happy New Year 2025 ${fVal} ${days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s "}`);
+        alert(`Happy New Year 2025 ${fVal}`);
     }
 }
