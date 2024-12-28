1be884d9b138808efa9057419c7d134d59ef3904
Debnath <166276275+Debnath225@users.noreply.github.com>
2024-12-28 16:28:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 const fName = document.getElementById("fName");
 
-const lName = document.getElementById("lName").innerHTML;
-console.log(lName);
+const lName = document.getElementById("lName");
+
+const getValue = document.getElementById("getValue");
+const noEntry = document.getElementById("noEntry");
 
 
+function onSubmit(){
+    if(fName.value==0&&lName.value==0){
+        noEntry.innerHTML="Access denied";
+        return 0;
+    }
+    else{
+        getValue.action="/Open_Page/open.html";
 
-const ufName = fName.value;
-const ulName =lName.value;
+    }
+}
